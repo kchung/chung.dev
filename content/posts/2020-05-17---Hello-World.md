97eeb286b2c71d1bdb6a7bb2c8703c1c915ce7df
Kevin Chung <kevin@chun.gd>
2020-05-17 15:20:08 -0700

    Remove placeholder data

---
+++ content/posts/2020-05-17---Hello-World.md
@@ -0,0 +1,12 @@
+---
+title: Hello World
+date: "2020-05-17T21:37:10.029Z"
+template: "post"
+draft: false
+slug: "hello-world"
+category: "Small Talk"
+tags:
+  - No Tag
+description: ""
+socialImage: ""
+---
--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,6 @@
 'use strict';
 
 require('./src/assets/scss/init.scss');
-require('./static/css/prismjs/theme.min.css');
+require('./static/css/prismjs/prism-atom-dark.css');
+
+require('./src/assets/scss/custom.scss');
